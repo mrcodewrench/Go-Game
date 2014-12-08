c682372695779d3f0775feb1c8c1ec2d30466475
Subletter <kevinhasfriends@gmail.com>
2014-12-08 00:58:09 -0800

    adding example of custom matcher

--- spec/Display.blankBoard.spec.js
+++ spec/Display.blankBoard.spec.js
@@ -1,4 +1,28 @@
 describe("blankboard function", function() {
+    beforeEach(function() {
+      jasmine.addMatchers({
+        toBeSameBoardstateAs: function() {
+          return {
+            compare: function(actual, expected) {
+              var result = {pass: true};
+
+              for (var i = 0; i < actual.length; i++)
+                for (var j = 0; j < actual.length; j++)
+                  if(actual[i][j] != expected[i][j])
+                    result.pass = false;
+
+              if(result.pass)
+                result.message = "I dk when we'd use this here..."
+              else
+                result.message = "They don't think it be like it is. But it do."
+
+              return result;
+            }
+          }
+        }
+      })
+    });
+
     it("should create a 9x9 board populated with 0", function() {
         var toCompare = [   ["0","0","0","0","0","0","0","0","0"],
                             ["0","0","0","0","0","0","0","0","0"],
@@ -9,7 +33,7 @@ describe("blankboard function", function() {
                             ["0","0","0","0","0","0","0","0","0"],
                             ["0","0","0","0","0","0","0","0","0"],
                             ["0","0","0","0","0","0","0","0","0"],];
-        var board = go.blankBoard();
-        expect(logic.boardMatcher(toCompare,board)).toBe(true);
+
+        expect(toCompare).toBeSameBoardstateAs(new Display().blankBoard());
     });
 });
